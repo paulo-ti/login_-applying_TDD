8bcd3cbcaf6d48cc8d76b95dfafc25d2dfe71607
paulo-ti <paulo-ti@hotmail.com>
2020-06-22 21:39:35 -0300

    creating files for test by Jest

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password_hash: DataTypes.String,
+    password_hash: DataTypes.STRING,
   });
   return User;
 };
